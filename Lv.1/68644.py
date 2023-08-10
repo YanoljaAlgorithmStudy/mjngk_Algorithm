eb64a7058ba5b95cc35e5e6a10e5298a88d342be
minjung <ypd06021@naver.com>
2023-08-10 17:17:27 +0900

    0810: 레벨1 - 두 개 뽑아서 더하기

---
+++ Lv.1/68644.py
@@ -0,0 +1,13 @@
+#0810
+# 두 개 뽑아서 더하기 
+
+from itertools import combinations
+
+def solution(numbers):
+    answer = []
+    arr=list(combinations(numbers, 2))
+    for a, b in arr:
+        answer.append(a+b)
+    answer=list(set(answer))
+    answer.sort()
+    return answer
