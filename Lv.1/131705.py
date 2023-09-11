8431476e01f49e7f4cdebdadcf27cb80769d91fe
minjung <ypd06021@naver.com>
2023-09-11 23:36:23 +0900

    0911 : 레벨1 - 삼총사

---
+++ Lv.1/131705.py
@@ -0,0 +1,11 @@
+#0911
+#삼총사
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    arr= list(combinations(number, 3))
+    for i in arr:
+        if sum(i)==0:
+            answer+=1
+    return answer
